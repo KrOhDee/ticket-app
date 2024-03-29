6eb4e1ca72a10413ff99ce7383aa91bf0b6dfee6
Luis Aleman <lalem014@fiu.edu>
2024-03-28 23:57:26 -0400

    Add app name

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,4 +1,12 @@
-import { faHome, faTicket } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome,
+  faTicket,
+  faScrewdriver,
+  faToolbox,
+  faCog,
+  faShieldAlt,
+  faWrench,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
@@ -11,6 +19,14 @@ const Nav = () => {
         </Link>
         <Link href="/TicketPage/new">
           <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div className="flex gap-1 items-center">
+        <Link href="/" style={{ display: 'contents' }}>
+          <p className="text-default-text text-[1.1rem] p-0 m-0">
+            Fix <span className="text-red-400">Fleet</span>
+          </p>
+          <FontAwesomeIcon className="text-white" icon={faShieldAlt} />
         </Link>
       </div>
       <div>
