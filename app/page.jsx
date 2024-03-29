026de2d523a7ca9ffbf297844434eed11e43a225
Luis Aleman <lalem014@fiu.edu>
2024-03-29 13:15:52 -0400

    Set up for deployment

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,12 @@ import TicketCard from './(components)/TicketCard';
 
 const getTickets = async () => {
   try {
-    const res = await fetch('http:localhost:3000/api/Tickets', {
-      cache: 'no-store',
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/Tickets`,
+      {
+        cache: 'no-store',
+      }
+    );
 
     return res.json();
   } catch (error) {
@@ -21,25 +24,32 @@ const Dashboard = async () => {
 
   return (
     <div className="p-5">
-      <div>
-        {tickets &&
-          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
-            <div key={categoryIndex} className="mb-4">
-              <h2>{uniqueCategory}</h2>
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-                {tickets
-                  .filter((ticket) => ticket.category === uniqueCategory)
-                  .map((filteredTicket, _index) => (
-                    <TicketCard
-                      id={_index}
-                      key={_index}
-                      ticket={filteredTicket}
-                    />
-                  ))}
-              </div>
+      {tickets && tickets.length > 0 ? (
+        uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div key={categoryIndex} className="mb-4">
+            <h2>{uniqueCategory}</h2>
+            <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+              {tickets
+                .filter((ticket) => ticket.category === uniqueCategory)
+                .map((filteredTicket, _index) => (
+                  <TicketCard
+                    id={_index}
+                    key={_index}
+                    ticket={filteredTicket}
+                  />
+                ))}
             </div>
-          ))}
-      </div>
+          </div>
+        ))
+      ) : (
+        <div className="text-center">
+          <h2>No tickets to display</h2>
+          <p>
+            Please check back later or create a new ticket by clicking the
+            ticket icon in the navigation bar.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
